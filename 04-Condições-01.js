26c60022a7e8f09496ea0fc288e88f92a7d28268
Matheus Paizante <matheus_paizante@hotmail.com>
2022-12-28 15:09:34 -0300

    .

---
+++ 04-Condições-01.js
@@ -0,0 +1,8 @@
+console.log('O console funiconou corretamente')
+var vel = 30
+console.log(`A velocidade do seu carro é ${vel}km/h`)
+if (vel>60){
+    console.log(`A velocidade do seu carro é ${vel}km/h. Multado.`)
+}
+
+console.log(`Dirija com segurança.`)  
