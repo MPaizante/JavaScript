bd8b4fdd89ac198926ec27d85669ab4aa9ef21c5
Matheus Paizante <matheus_paizante@hotmail.com>
2022-12-29 16:17:29 -0300

    .

--- 06-Ex-02-01.js
+++ 06-Ex-02-01.js
@@ -10,20 +10,25 @@ if (fano.value.length == 0 || Number(fano.value) > ano){
                 var fsex = window.document.getElementsByName('radsex')
                 var idade = ano - Number(fano.value)
                 var genero = ''
-            img.setAttribute ('id', 'foto')
                 var img = document.createElement('img')
+            img.setAttribute ('id', 'foto')
+            
+                
 
 
 if(fsex[0].checked){
         genero = 'Homem'
         if (idade >=0 && idade < 10){
+            img.setAttribute('src' , '../baby-laughing-icon (1).png' )
 
         } else if (idade <21){
+            img.setAttribute('src' , '../Male-Face-B5-icon.png' )
 
         } else if (idade < 50){
-
+            img.setAttribute('src' , '../Office-Customer-Male-Light-icon.png' )    
         } 
     else{
+        img.setAttribute('src' , '../Old-Boss-icon.png' )
 
         }
         
@@ -32,20 +37,25 @@ if(fsex[0].checked){
     }else if (fsex[1].checked){
         genero = 'Mulher'
         if (idade >=0 && idade < 10){
+            img.setAttribute('src' , 'baby-laughing-icon (1).png' )
 
         } else if (idade <21){
+            img.setAttribute('src' , 'browser-girl-firefox-icon.png' )
 
         } else if (idade < 50){
+            img.setAttribute('src' , 'Office-Girl-icon.png' )
             
         }
     else{
-        
+        img.setAttribute('src' , '10177-old-woman-light-skin-tone-icon.png' )
+
     }
         
 
     }
             res.style.textAling = 'center'
             res.innerHTML = `Detectmos ${genero} com ${idade} anos.` 
+            res.appendChild(img)
     
 
 
