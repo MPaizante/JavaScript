4810a69f26182de0a213dda81a1f33ac658f35d4
Matheus Paizante <matheus_paizante@hotmail.com>
2022-12-30 17:26:30 -0300

    Update 07-Repetições-01.js

--- 07-Repetições-01.js
+++ 07-Repetições-01.js
@@ -1,7 +1,19 @@
 
-
+var a = 1
 var c = 1 
+var b = 1
     do {
         console.log (`Passo ${c}`)
         c++
     } while (c<=10 )
+
+for (var a = 1; a <= 10;a++   ){
+    console.log (`Contando ${a}`)
+}
+
+while (b <= 5) {
+    console.log(b)
+    b++
+
+
+}
