3fd4cb730a7eb7dcf2595fe4970223c236af213a
Matheus Paizante <matheus_paizante@hotmail.com>
2023-01-02 15:08:29 -0300

    .

---
+++ 07-Ex-03-01.js
@@ -0,0 +1,24 @@
+function contar(){
+
+    let ini = document.getElementById('txti')
+    let fim = window.document.getElementById('txtf')
+    let passo =  document.getElementById('txtp')
+    let res = document.getElementById('res')
+
+    if (ini.value.length ==0 || fim.value.length == 0 || passo.value.length ==0){
+        window.alert('Erro')
+    } else{  
+        res.innerHTML = 'Contando: '
+        let i = Number(ini.value)
+        let f = Number(fim.value)
+        let p = Number(passo.value)
+        
+        for (let c = i ; c <= f ; c += p){
+            res.innerHTML += `  ${c}\u{1F60A}`
+
+        }
+
+        res.innerHTML += ` \u{1F680}`
+
+    }
+}
