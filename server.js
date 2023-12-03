d4c0278e7efef8f66e2efd9cf73f29546b73c8c7
Matheus Paizante <matheus_paizante@hotmail.com>
2023-12-03 20:18:49 -0300

    .

--- server.js
+++ server.js
@@ -1,8 +1,20 @@
 import app from './src/app.js'
+
+import conexao from './Infra/conexao.js'
+
 //Porta
 const PORT = 3000
 
-// escutar a porta 3000
-app.listen(PORT, () => {
-    console.log(`Servidor rondando no endereço http://localhost:${PORT}`)
+//fazer a conexao
+conexao.connect((erro) =>{
+    if(erro){
+        console.log(erro)
+    }else{
+        console.log("Conexão realizada com sucesso!")
+        // escutar a porta 3000
+        app.listen(PORT, () => {
+            console.log(`Servidor rondando no endereço http://localhost:${PORT}`)
+        })
+    }
 })
+
